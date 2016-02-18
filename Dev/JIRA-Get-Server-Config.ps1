a0e0d3d60e7decbcd4813233a5303877eaf949e8
Arnab Ghosal <arnabghosal@gmail.com>
2016-02-18 08:32:11 +0530

    Update on v0.1.4

---
+++ Dev/JIRA-Get-Server-Config.ps1
@@ -0,0 +1,55 @@
+﻿function JIRA-Get-Server-Config
+{
+	[CmdletBinding()]
+	param
+	(
+		[ValidateNotNullOrEmpty()]
+		[String] $Config = "JIRA-Config"
+	)
+
+	$FName = $MyInvocation.MyCommand
+	$XML_File = New-Object -TypeName XML
+
+	if (-not (Int-Config-Test -Folder))
+	{
+		Write-Verbose "[$FName] Config Folder Not Found"
+		throw "[$FName] Invalid Config Folder. Execute 'JIRA-Create-Server-Config'"
+	}
+	else
+	{
+		Write-Verbose "[$FName] Config Folder Found"
+	}
+
+	if (-not (Int-Config-Test -File -Config $Config))
+	{
+		Write-Verbose "[$FName] Config File Not Found"
+		throw "[$FName] Invalid Config File. Execute 'JIRA-Create-Server-Config'"
+	}
+	else
+	{
+		Write-Verbose "[$FName] Config File Found"
+	}
+
+	Write-Verbose "[$FName] Reading XML File"
+	$XML_File.Load($Config_File)
+	$Config_XML = $XML_File.DocumentElement
+
+	if($Config_XML.JIRA_Link)
+	{
+		[string]$JIRA_URL = $Config_XML.JIRA_Link
+		if ($JIRA_URL.EndsWith('/'))
+		{
+			$JIRA_URL = $JIRA_URL.TrimEnd('/')
+		}
+		Set-Variable -Name JIRA_Link -Value $JIRA_URL -Scope "Global" -Option Constant
+		Set-Variable -Name JIRA_Auth -Value $($JIRA_Link + "/rest/auth/1/session/") -Scope "Global" -Option Constant
+		Set-Variable -Name JIRA_API -Value $($JIRA_Link + "/rest/api/2/issue/") -Scope "Global" -Option Constant
+		Write-Verbose "[$FName] JIRA_Link Exported"
+	}
+	else
+	{
+		Write-Verbose "[$FName] Removing Invalid Config File"
+		Remove-Item -Path $Config_File -Force
+		throw "[$FName] Invalid Config File. Execute 'JIRA-Create-Server-Config'"
+	}
+}
---
+++ Dev/JIRA-Set-Server-Config.ps1
@@ -0,0 +1,69 @@
+﻿function JIRA-Set-Server-Config
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true, Position = 0)]
+		[ValidateNotNullOrEmpty()]
+		[String] $JIRA_URL,
+		[String] $Config = "JIRA-Config"
+	)
+
+	$FName = $MyInvocation.MyCommand
+	$XML_File = New-Object -TypeName XML
+
+	if (-not (Int-Config-Test -Folder))
+	{
+		Write-Verbose "[$FName] Config Folder Not Found"
+		throw "[$FName] Invalid Config Folder. Execute 'JIRA-Create-Server-Config'"
+	}
+	else
+	{
+		Write-Verbose "[$FName] Config Folder Found"
+	}
+
+	if (-not (Int-Config-Test -File -Config $Config))
+	{
+		Write-Verbose "[$FName] Config File Not Found"
+		throw "[$FName] Invalid Config File. Execute 'JIRA-Create-Server-Config'"
+	}
+	else
+	{
+		Write-Verbose "[$FName] Config File Found"
+	}
+
+	try
+	{
+		Write-Verbose "[$FName] Reading Config File"
+		$XML_File.Load($Config_File)
+		$Config_XML = $XML_File.DocumentElement
+	}
+	catch
+	{
+		Write-Verbose "Removing Invalid Config File"
+		Remove-Item -Path $Config_File -Force
+		throw "[$FName] Invalid Config File. Execute 'JIRA-Create-Server-Config'"
+	}
+
+	if ($Config_XML.Name -eq 'JIRA_Config')
+	{
+		if ([string]::IsNullOrEmpty($Config_XML.JIRA_Link))
+		{
+			$Config_XML.JIRA_Link = $JIRA_URL
+			Write-Verbose "[$FName] JIRA URL Initialised"
+		}
+		else
+		{
+			$Config_XML.JIRA_Link = $JIRA_URL
+			Write-Verbose "[$FName] JIRA URL Updated"
+		}
+		$XML_File.Save($Config_File)
+		Write-Verbose "[$FName] Config File Saved"
+	}
+	else
+	{
+		Write-Verbose "[$FName] Removing Invalid Config File"
+		Remove-Item -Path $Config_File -Force
+		throw "[$FName] Invalid Config File. Execute 'JIRA-Create-Server-Config'"
+	}
+}
